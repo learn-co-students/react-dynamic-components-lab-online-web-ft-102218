54b259e233bf44fe5d5e2b3a0bdb33a2d485f4eb
Eric Mayfield <eric@visiblycreative.com>
2019-05-08 19:24:43 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,9 @@
-// add Comment component here
+import React, { Component } from 'react';
+
+export default class ToDoList extends Component {
+    render() {
+        return (
+            <div className="comment">{this.props.commentText}</div>
+        )
+    }
+}
